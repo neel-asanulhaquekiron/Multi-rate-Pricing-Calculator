4400289f5f10ef495907e7342858c113ed8bfc77
neel-asanulhaquekiron <asanulhaquekiron@gmail.com>
2026-08-08 09:47:40 -0500

    feat: update import paths for App and AuthProvider components

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,8 +1,8 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import { App } from "./App";
-import { AuthProvider } from "./auth";
+import { App } from "@/app/App";
+import { AuthProvider } from "@/features/auth/AuthContext";
 import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
